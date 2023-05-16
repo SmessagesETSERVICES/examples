f65bd28a5f11b6b4e5dbfc1d9eb067ee3a56430f
Smessages <djatone@outlook.fr>
2023-05-16 08:07:01 +0200

    yep

---
+++ marvel_api/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify 
+import time 
+import hashlib, requests
+from keys import PUBLIC_KEY, PRIVATE_KEY
+
+app = Flask(__name__)
+PUBLIC_KEY = PUBLIC_KEY
+PRIVATE_KEY = PRIVATE_KEY
+BASE_URL = 'https://gateway.marvel.com/v1/public/'
+
+def generate_hash(ts, private_key, public_key):
+    m = hashlib.md5()
+    m.update(f"{ts}{private_key}{public_key}".encode('utf-8'))
+    return m.hexdigest()
+
+@app.route('/characters')
+def get_characters():
+    ts = str(time.time())
+    hash = generate_hash(ts, PRIVATE_KEY, PUBLIC_KEY)
+    params = {
+        'apikey': PUBLIC_KEY,
+        'ts': ts,
+        'hash': hash,
+        'limit': 100
+    }
+    response = requests.get(f"{BASE_URL}characters", params=params)
+    return jsonify(response.json())
+
+app.run(debug=True)
---
+++ marvel_api/happi.py
@@ -0,0 +1,15 @@
+from marvel import Marvel
+
+from keys import PRIVATE_KEY, PUBLIC_KEY
+
+
+m = Marvel(PUBLIC_KEY=PUBLIC_KEY, 
+           PRIVATE_KEY=PRIVATE_KEY)
+
+characters = m.characters
+
+my_char = characters.all(name="black bird")["data"]["results"]
+
+print(my_char[0]["series"]["collectionURI"])
+
+
